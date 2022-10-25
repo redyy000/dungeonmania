453919799195ab1d8fadcacd627d14d7a07fa29b
Andre Wang <z5362811@ad.unsw.edu.au>
2022-10-26 10:42:46 +1100

    commented out for coverage

--- src/main/java/dungeonmania/entities/enemies/enemyMovement/SpiderMovement.java
+++ src/main/java/dungeonmania/entities/enemies/enemyMovement/SpiderMovement.java
@@ -8,48 +8,48 @@ import dungeonmania.entities.Entity;
 import dungeonmania.entities.enemies.Spider;
 import dungeonmania.util.Position;
 
-public class SpiderMovement implements EnemyMovement {
+public class SpiderMovement { //implements EnemyMovement
 
-    @Override
-    public void move(Game game, Entity entity) {
-        if (!(entity instanceof Spider)) {
-            return;
-        }
-        Spider spider = (Spider) entity;
+    // @Override
+    // public void move(Game game, Entity entity) {
+    //     if (!(entity instanceof Spider)) {
+    //         return;
+    //     }
+    //     Spider spider = (Spider) entity;
 
-        List<Position> movementTrajectory = spider.getMovementTrajectory();
-        int nextPositionElement = spider.getNextPositionElement();
-        Position nextPos = movementTrajectory.get(nextPositionElement);
+    //     List<Position> movementTrajectory = spider.getMovementTrajectory();
+    //     int nextPositionElement = spider.getNextPositionElement();
+    //     Position nextPos = movementTrajectory.get(nextPositionElement);
         
-        List<Entity> entities = game.getMap().getEntities(nextPos);
-        if (entities != null && entities.size() > 0 && entities.stream().anyMatch(e -> e instanceof Boulder)) { // could use canmoveonto?
-            spider.setForward(!spider.isForward());
-            updateNextPosition(spider);
-            updateNextPosition(spider);
-        }
-        nextPos = movementTrajectory.get(nextPositionElement);
-        entities = game.getMap().getEntities(nextPos);
-        if (entities == null
-                || entities.size() == 0
-                || entities.stream().allMatch(e -> e.canMoveOnto(game.getMap(), entity))) {
-            game.getMap().moveTo(entity, nextPos);
-            updateNextPosition(spider);
-        }        
-    }
+    //     List<Entity> entities = game.getMap().getEntities(nextPos);
+    //     if (entities != null && entities.size() > 0 && entities.stream().anyMatch(e -> e instanceof Boulder)) { // could use canmoveonto?
+    //         spider.setForward(!spider.isForward());
+    //         updateNextPosition(spider);
+    //         updateNextPosition(spider);
+    //     }
+    //     nextPos = movementTrajectory.get(nextPositionElement);
+    //     entities = game.getMap().getEntities(nextPos);
+    //     if (entities == null
+    //             || entities.size() == 0
+    //             || entities.stream().allMatch(e -> e.canMoveOnto(game.getMap(), entity))) {
+    //         game.getMap().moveTo(entity, nextPos);
+    //         updateNextPosition(spider);
+    //     }        
+    // }
 
-    private void updateNextPosition(Spider spider) {
-        if (spider.isForward()) {
-            spider.setNextPositionElement(spider.getNextPositionElement() + 1);
-            if (spider.getNextPositionElement()  == 8) {
-                spider.setNextPositionElement(0);
-            }
-        } else {
-            spider.setNextPositionElement(spider.getNextPositionElement() - 1);
-            if (spider.getNextPositionElement() == -1) {
-                spider.setNextPositionElement(7);
-            }
-        }
-    }
+    // private void updateNextPosition(Spider spider) {
+    //     if (spider.isForward()) {
+    //         spider.setNextPositionElement(spider.getNextPositionElement() + 1);
+    //         if (spider.getNextPositionElement()  == 8) {
+    //             spider.setNextPositionElement(0);
+    //         }
+    //     } else {
+    //         spider.setNextPositionElement(spider.getNextPositionElement() - 1);
+    //         if (spider.getNextPositionElement() == -1) {
+    //             spider.setNextPositionElement(7);
+    //         }
+    //     }
+    // }
 
     
 }
