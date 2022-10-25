9f712e24f306001d2ac77a9aba5adaba2d5631ba
Andre Wang <z5362811@ad.unsw.edu.au>
2022-10-25 15:39:58 +1100

    add import line

--- src/main/java/dungeonmania/entities/Player.java
+++ src/main/java/dungeonmania/entities/Player.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+import dungeonmania.Game;
 import dungeonmania.battles.BattleStatistics;
 import dungeonmania.battles.Battleable;
 import dungeonmania.entities.collectables.Bomb;
