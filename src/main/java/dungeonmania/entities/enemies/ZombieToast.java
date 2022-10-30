7cf6d273fd825250d4acd451a14969866ea83f56
Andre Wang <andre.wang@student.unsw.edu.au>
2022-10-30 14:36:37 +1100

    Task 1a

--- src/main/java/dungeonmania/entities/enemies/ZombieToast.java
+++ src/main/java/dungeonmania/entities/enemies/ZombieToast.java
@@ -1,17 +1,16 @@
 package dungeonmania.entities.enemies;
 
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
 
 import dungeonmania.Game;
-import dungeonmania.map.GameMap;
+import dungeonmania.entities.enemies.enemyMovement.EnemyMovement;
+import dungeonmania.entities.enemies.enemyMovement.RandomMovement;
 import dungeonmania.util.Position;
 
 public class ZombieToast extends Enemy {
     public static final double DEFAULT_HEALTH = 5.0;
     public static final double DEFAULT_ATTACK = 6.0;
-    private Random randGen = new Random();
+
+    private EnemyMovement moveStrategy = new RandomMovement();
 
     public ZombieToast(Position position, double health, double attack) {
         super(position, health, attack);
@@ -19,20 +18,7 @@ public class ZombieToast extends Enemy {
 
     @Override
     public void move(Game game) {
-        Position nextPos;
-        GameMap map = game.getMap();
-        List<Position> pos = getPosition().getCardinallyAdjacentPositions();
-        pos = pos
-            .stream()
-            .filter(p -> map.canMoveTo(this, p)).collect(Collectors.toList());
-        if (pos.size() == 0) {
-            nextPos = getPosition();
-            game.getMap().moveTo(this, nextPos);
-        } else {
-            nextPos = pos.get(randGen.nextInt(pos.size()));
-            game.getMap().moveTo(this, nextPos);
-        }
-
+        this.moveStrategy.move(game, this);
     }
 
 }
---
+++ src/main/java/dungeonmania/entities/enemies/enemyMovement/EnemyMovement.java
@@ -0,0 +1,8 @@
+package dungeonmania.entities.enemies.enemyMovement;
+
+import dungeonmania.Game;
+import dungeonmania.entities.Entity;
+
+public interface EnemyMovement {
+    public void move(Game game, Entity entity);
+}
