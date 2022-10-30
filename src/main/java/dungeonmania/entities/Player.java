b11be645db95056faa7a92d17c2913445764fe28
Andre Wang <z5362811@ad.unsw.edu.au>
2022-10-30 15:38:33 +1100

    remove more useless overrides

--- src/main/java/dungeonmania/entities/Player.java
+++ src/main/java/dungeonmania/entities/Player.java
@@ -178,17 +178,6 @@ public class Player extends Entity implements Battleable {
         }
         return origin;
     }
-
-    @Override
-    public void onMovedAway(GameMap map, Entity entity) {
-        return;
-    }
-
-    @Override
-    public void onDestroy(GameMap gameMap) {
-        return;
-    }
-
     @Override
     public double getHealth() {
         return getBattleStatistics().getHealth();
