90728ff4e3115dec26ab71998c107ccc8550db8d
Webster Zhang <webster.zhang@unsw.edu.au>
2022-11-01 13:22:53 +0800

    Assignment II Automarking Dryrun Results

--- src/main/java/dungeonmania/battles/Battleable.java
+++
@@ -1,9 +0,0 @@
-package dungeonmania.battles;
-
-/**
- * Entities implement this interface can do battles
- */
-public interface Battleable {
-    public BattleStatistics getBattleStatistics();
-    public double getHealth();
-}
--- src/main/java/dungeonmania/entities/enemies/enemyMovement/EnemyMovement.java
+++
@@ -1,8 +0,0 @@
-package dungeonmania.entities.enemies.enemyMovement;
-
-import dungeonmania.Game;
-import dungeonmania.entities.Entity;
-
-public interface EnemyMovement {
-    public void move(Game game, Entity entity);
-}
