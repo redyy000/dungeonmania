7ea991f52eb29f6357b5b3111a8035bf3a5cc36b
Andre Wang <z5362811@ad.unsw.edu.au>
2022-11-02 11:49:24 +1100

    add another test for persistence

--- src/test/java/dungeonmania/mvp/PersistenceTest.java
+++ src/test/java/dungeonmania/mvp/PersistenceTest.java
@@ -72,13 +72,46 @@ public class PersistenceTest {
         //move right twice.
         res = dmc.tick(Direction.RIGHT);
         res = dmc.tick(Direction.RIGHT);
-        res = dmc.loadGame("testSavePos");
 
-        //assert player is at the position when saved.
+        //assert player is at the position as when they saved.
+        res = dmc.loadGame("testSavePos");
         assertEquals(posAtSave, TestUtils.getPlayerPos(res));
 
         // assert movement is still correct
         res = dmc.tick(Direction.DOWN);
         assertEquals(new Position(2, 2), TestUtils.getPlayerPos(res));
+    }
+
+    @Test
+    @DisplayName("Test saving then loading and exit goal still works.")
+    public void loadExitGoal() {
+        DungeonManiaController dmc;
+        dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame("d_basicGoalsTest_exit", "c_basicGoalsTest_exit");
+
+        // move player to right
+        res = dmc.tick(Direction.RIGHT);
+        // assert goal not met
+        assertTrue(TestUtils.getGoals(res).contains(":exit"));
+
+
+        //Save game
+        Position posAtSave = TestUtils.getPlayerPos(res); //should be x=2, y =1 at save.
+        res = assertDoesNotThrow(() -> dmc.saveGame("testExit"));
+        try {        // Wait for a bit to let the thing write.
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Load game, assert player is at the position as when they saved, and goal not met.
+        res = dmc.loadGame("testExit");
+        assertEquals(posAtSave, TestUtils.getPlayerPos(res));
+        assertTrue(TestUtils.getGoals(res).contains(":exit"));
+
+
+        // move player to exit        // assert goal met
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals("", TestUtils.getGoals(res));
     }
 }
