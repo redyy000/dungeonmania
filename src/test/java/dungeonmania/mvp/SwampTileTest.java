dd6dc3351b589853179415e27dac5d2d67201f72
Ryan Godakanda <ryangodakanda@gmail.com>
2022-11-05 20:02:02 +1100

    Added swamp tile test for assassins

--- src/test/java/dungeonmania/mvp/SwampTileTest.java
+++ src/test/java/dungeonmania/mvp/SwampTileTest.java
@@ -37,7 +37,37 @@ public class SwampTileTest {
         assertEquals(new Position(6, 1), getMercPos(res));
     }
 
+    @Test
+    @DisplayName("Testing an assassin gets slowed. Factor 2")
+    public void assassinSlowed() {
+        //                                                          Wall     Wall     Wall    Wall    Wall
+        // P1       P2/               P3/                P4/      M4       M3       M2     M1      Wall
+        //                                                          Wall     Wall     Wall    Wall    Wall
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame("d_swampTileAssassin", "c_assassinWillBeBribed");
+
+        // get him on tile
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(new Position(7, 1), getAssPos(res));
+
+        // 1 tick cant move
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(new Position(7, 1), getAssPos(res));
+
+        // 2 tick cant move
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(new Position(7, 1), getAssPos(res));
+
+        // 3 tick can move
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(new Position(6, 1), getAssPos(res));
+    }
+
     private Position getMercPos(DungeonResponse res) {
         return TestUtils.getEntities(res, "mercenary").get(0).getPosition();
+    }
+
+    private Position getAssPos(DungeonResponse res) {
+        return TestUtils.getEntities(res, "assassin").get(0).getPosition();
     }
 }
