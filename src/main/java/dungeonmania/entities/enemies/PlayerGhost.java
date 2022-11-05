315e145802d299ad402d2833273621b498f3db0d
Andre Wang <z5362811@ad.unsw.edu.au>
2022-11-05 19:30:49 +1100

    make one file

---
+++ src/main/java/dungeonmania/entities/enemies/PlayerGhost.java
@@ -0,0 +1,13 @@
+package dungeonmania.entities.enemies;
+
+import dungeonmania.Game;
+
+public class PlayerGhost extends Enemy  {
+
+    @Override
+    public void move(Game game) {
+        
+        
+    }
+    
+}
